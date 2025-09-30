d0cddb8492371ccaf625dc43252bc41c4ee902f0
coli <kkwoo001021@naver.com>
2025-10-01 05:06:24 +0900

    chore: ci-pipeline test1

--- tests/test_example.py
+++ tests/test_example.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/ping")
     assert response.status_code == 200
-    assert response.json() == {"ping": "pong1"}
+    assert response.json() == {"ping": "pong"}
